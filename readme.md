a0609fa964062279bed0dc481e4735c80f815400
bembee <bembee@freemail.hu>
2020-03-27 12:33:29 +0100

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,8 @@
+A szkript **Firefox**,  **Chrome**, **Microsoft Edge**, **Microsoft Edge (Chromium)**, **Opera (béta)** böngészőkre telepíthető.<br>
+A telepítéssel elfogadod a [használat feltételeit](https://raw.githubusercontent.com/bembee/SZAMALK_etananyag_orak_SZF1-13-B/master/LICENSE.txt).
+
+## Telepítés
+
+1. Telepítsd a TamperMonkey kiegészítőt [Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey), [Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo), [Microsoft Edge](https://www.microsoft.com/store/apps/9NBLGGH5162S), [Microsoft Edge (Chromium)](https://microsoftedge.microsoft.com/insider-addons/detail/iikmkjmpaadaobahmlepeloendndfphd?hl=en-US), [Opera (béta)](https://addons.opera.com/hu/extensions/details/tampermonkey-beta)
+1. Telepítsd a szkriptet. [Kattints ide](https://github.com/bembee/SZAMALK_etananyag_orak_SZF1-13-B/raw/master/szamalk.user.js)
+1. A megjelenő oldalon kattints az **Install**/**Telepítés** gombra.
---
+++ szamalk.user.js
@@ -0,0 +1,45 @@
+﻿// ==UserScript==
+// @name           SZÁMALK etananyag órák SZF1/13/B!
+// @author         Simon Gergely
+// @description    Menüpontot készít a tényleges óráninkról
+// @version        0.9
+// @include        https://*etananyag.szamalk-szalezi*
+// ==/UserScript==
+
+var ul = document.querySelector('.dropdown-menu');
+var egy = document.createElement('li');
+egy.innerHTML = '<a title="" class="dropdown-item" href="https://etananyag.szamalk-szalezi.hu/course/view.php?id=13">Adatbázis alkalmazások E+GY</a>';
+ul.appendChild(egy);
+
+
+var ketto = document.createElement('li');
+ketto.innerHTML = '<a title="" class="dropdown-item" href="https://etananyag.szamalk-szalezi.hu/course/view.php?id=20">Etika</a>';
+ul.appendChild(ketto);
+
+var harom = document.createElement('li');
+harom.innerHTML = '<a title="" class="dropdown-item" href="https://etananyag.szamalk-szalezi.hu/course/view.php?id=9">Hálózatok I.</a>';
+ul.appendChild(harom);
+
+var negy = document.createElement('li');
+negy.innerHTML = '<a title="" class="dropdown-item" href="https://etananyag.szamalk-szalezi.hu/course/view.php?id=10">Hálózatok I. gyakorlat</a>';
+ul.appendChild(negy);
+
+var ot = document.createElement('li');
+ot.innerHTML = '<a title="" class="dropdown-item" href="https://etananyag.szamalk-szalezi.hu/course/view.php?id=6">IT alapok</a>';
+ul.appendChild(ot);
+
+var hat = document.createElement('li');
+hat.innerHTML = '<a title="" class="dropdown-item" href="https://etananyag.szamalk-szalezi.hu/course/view.php?id=7">IT alapok gyakorlat</a>';
+ul.appendChild(hat);
+
+var het = document.createElement('li');
+het.innerHTML = '<a title="" class="dropdown-item" href="https://etananyag.szamalk-szalezi.hu/course/view.php?id=18">IT Szakmai angol</a>';
+ul.appendChild(het);
+
+var nyolc = document.createElement('li');
+nyolc.innerHTML = '<a title="" class="dropdown-item" href="https://etananyag.szamalk-szalezi.hu/course/view.php?id=12">Programozás</a>';
+ul.appendChild(nyolc);
+
+var kilenc = document.createElement('li');
+kilenc.innerHTML = '<a title="" class="dropdown-item" href="https://etananyag.szamalk-szalezi.hu/course/view.php?id=472">Webprogramozás</a>';
+ul.appendChild(kilenc);
